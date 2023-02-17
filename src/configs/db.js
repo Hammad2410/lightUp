e57649c83e9a15aac4b510807b48eb219f23b70d
Hammad2410 <Hammad.saeed2410@gmail.com>
2023-02-17 22:39:50 +0500

    db changed

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ const { Client } = require('pg');
 
 const connection = new Client({
     user: "postgres",
-    password: "VpbVQVclj15j0mLVEkyK",
+    password: "1DExLV2VamFgUhGEdN6Z",
     database: "postgres",
     port: 5432,
-    host: "light-up.crfv5c69uecj.ap-south-1.rds.amazonaws.com",
+    host: "lightup.coiyz6ujjlys.ap-south-1.rds.amazonaws.com",
     ssl: { rejectUnauthorized: false }
 });
 
