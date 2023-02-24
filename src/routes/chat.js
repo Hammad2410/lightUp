b7218ba896d77f71ef4b23eef7cf73ec2b3692a7
Hammad2410 <Hammad.saeed2410@gmail.com>
2023-02-24 23:47:44 +0500

    minor changes

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const openai = require('.././configs/openai')
 const connection = require('../configs/db')
+const WordExtractor = require("word-extractor");
+const upload = require('../configs/upload');
+const extractor = new WordExtractor();
 
 const chatRoute = express.Router()
 
@@ -77,6 +80,154 @@ chatRoute.post('/createTest', async (req, res) => {
     }
 
 })
+
+chatRoute.post('/answerQuestion', async (req, res) => {
+    try {
+        let { question } = req.body;
+
+        // console.log(req.body)
+
+        let result = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${question}`,
+            max_tokens: 50,
+            temperature: 0,
+        });
+
+
+        if (result) {
+            console.log("Working ", result?.data)
+
+            res.send({
+                success: true,
+                message: 'Response fetched',
+                result: result?.data?.choices[0]?.text
+            })
+        }
+
+    }
+    catch (error) {
+
+        console.warn(error)
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+chatRoute.post('/createQuestions', async (req, res) => {
+    try {
+        let { topic, questions } = req.body;
+
+        // console.log(req.body)
+
+        let result = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Create ${questions} questions from ${topic}`,
+            max_tokens: 50,
+            temperature: 0,
+        });
+
+
+        if (result) {
+            console.log("Working ", result?.data)
+
+            res.send({
+                success: true,
+                message: 'Response fetched',
+                result: result?.data?.choices[0]?.text
+            })
+        }
+
+    }
+    catch (error) {
+
+        console.warn(error)
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+chatRoute.post('/summerize', upload.single('file'), async (req, res) => {
+    try {
+        let { text, words, url } = req.body;
+
+        let content = await extractor.extract(req.file.path)
+
+        // console.log(content.getBody())
+
+        let result = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Summarize ${content.getBody()} in ${words} words`,
+            max_tokens: +words,
+            temperature: 0,
+        });
+
+
+        if (result) {
+            console.log("Working ", result?.data)
+
+            res.send({
+                success: true,
+                message: 'Response fetched',
+                result: result?.data?.choices[0]?.text
+            })
+        }
+
+    }
+    catch (error) {
+
+        console.warn(error)
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+chatRoute.post('/solve', async (req, res) => {
+    try {
+        let { question } = req.body;
+
+        // console.log(req.body)
+
+        let result = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `Solve ${question} step by step`,
+            max_tokens: 250,
+            temperature: 0,
+        });
+
+
+        if (result) {
+            console.log("Working ", result?.data)
+
+            res.send({
+                success: true,
+                message: 'Response fetched',
+                result: result?.data?.choices[0]?.text
+            })
+        }
+
+    }
+    catch (error) {
+
+        console.warn(error)
+        res.send({
+            success: false,
+            message: error.message
+        })
+    }
+
+})
+
+
 
 // chatRoute.post('/createTest', async (req, res) => {
 //     try {
