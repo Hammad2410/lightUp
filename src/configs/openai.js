633ac6ce4172b37afa2df2a10fe99e104de28a13
Hammad2410 <Hammad.saeed2410@gmail.com>
2023-05-03 22:12:46 +0500

    git ignore fixed

--- src/configs/openai.js
+++
@@ -1,7 +0,0 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-
-module.exports = openai;
